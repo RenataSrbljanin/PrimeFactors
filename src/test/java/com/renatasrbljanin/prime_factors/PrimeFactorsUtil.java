9a7a93b12db5591a7ca327956054bb811b15d49c
Renata Srbljanin <renatasrbljanin@gmail.com>
2017-10-26 19:25:15 +0200

    Initial commit

---
+++ src/test/java/com/renatasrbljanin/prime_factors/PrimeFactorsUtil.java
@@ -0,0 +1,20 @@
+package com.renatasrbljanin.prime_factors;
+
+import java.util.Scanner;
+
+public class PrimeFactorsUtil {
+
+	public static void main(String[] args) {
+
+		int n;
+		System.out.println("Enter an integer to get it's prime factors ");
+		Scanner in = new Scanner(System.in);
+		
+		n = in.nextInt();
+		
+		System.out.println(PrimeFactors.of(n));
+		
+		in.close();
+	}
+
+}
